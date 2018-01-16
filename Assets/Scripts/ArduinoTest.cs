844b009d9252e3640fbf54e5b29a52e9c38d2b65
Ashley Cheema <ashleyycheemaa@hotmail.com>
2018-01-16 15:51:35 +0000

    Added Ardunio button controls

---
+++ Assets/Scripts/ArduinoTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.IO.Ports;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+
+
+
+public class ArduinoTest : MonoBehaviour
+{
+    public float speed;
+    private float amountToMove;
+
+    SerialPort serialPort = new SerialPort("COM9", 9600);
+
+    private void Start()
+    {
+        serialPort.Open();
+        serialPort.ReadTimeout = 1;
+    }
+
+    void Update()
+    {
+        amountToMove = speed * Time.deltaTime;
+
+        if(serialPort.IsOpen)
+        {
+            try
+            {
+                MoveObject(serialPort.ReadByte());
+                print(serialPort.ReadByte());
+            }
+            catch(System.Exception)
+            {
+
+            }
+        }
+    }
+
+    void MoveObject(int Direction)
+    {
+        if(Direction == 1)
+        {
+            transform.Translate(Vector3.up * amountToMove, Space.World);
+        }
+        if(Direction == 2)
+        {
+            transform.Translate(Vector3.down * amountToMove, Space.World);
+        }
+    }
+}
+
